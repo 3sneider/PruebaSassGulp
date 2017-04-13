1e05058ee49350256687c35b9ad1273c8f7f23d6
3sneider <dubier1992@gmail.com>
2017-04-13 18:20:07 -0500

    sass & glup funcionando en sincronia

--- src/gulp/gulpfile.js
+++ src/gulp/gulpfile.js
@@ -1,7 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({ server: "../../apps"});
+    gulp.watch("../sass/**/*.sass", ['sass']); // observa cambios y ejecuta la tarea
+    gulp.watch("../../apps/**/*.html").on('change', browserSync.reload); // recarga html en caliente
+});
 
 gulp.task('sass', function(){
-    console.log("funcionaa");
+    return gulp.src('../sass/main.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('../../apps/Themes/css'))
+        .pipe(browserSync.stream());
 });
