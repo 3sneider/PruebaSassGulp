0c2229605fcee8d371847b43c379f7088e788e6f
3sneider <dubier1992@gmail.com>
2017-04-17 00:04:34 -0500

    mejorando el codigo de prueba de angular usando factorys y services

--- apps/controller.js
+++ apps/controller.js
@@ -1,11 +1,67 @@
 var app = angular.module("ToDo", ["LocalStorageModule"]);
 
-app.controller("todoController", function($scope, $http, localStorageService){
+app.factory('todoService', function(localStorageService){ // una factory es una clase
+    var todoService = {}; // que contiene objetos que van a ser retornados
+    todoService.key = "clave";
+    if(localStorageService.get(todoService.key)){
+        todoService.activities = localStorageService.get(todoService.key);
+    }
+    else{
+        todoService.activities = [];
+    }
+    todoService.UpdaLocalStorage = function(){ // estos metodos van en el objeto
+        localStorageService.set(todoService.key, todoService.activities);
+    };
+    todoService.add = function(td){ // y metodos que reciven parametros
+        todoService.activities.push(td);
+        todoService.UpdaLocalStorage();
+    };
+    todoService.clean = function(){
+        todoService.activities = [ ];
+        todoService.UpdaLocalStorage();
+        return todoService.getAll();
+    };
+    todoService.getAll = function(){
+        return todoService.activities;
+    };
+    todoService.removeItem = function(item){
+        todoService.activities = todoService.activities.filter(function(act){
+            return act !== item;
+        });
+        todoService.UpdaLocalStorage();
+        return todoService.getAll();
+    };
+
+    return todoService; // devolbemos un objeto con los metodos creados
+});
+
+app.service('personService', function(){ // no retorna, ses mas un constructor
+
+    this.todos = [ ] ;
+
+    this.add = function(todo){ // y metodos que reciven parametros
+        this.todos.push(todo);
+    };
+    this.clean = function(){
+        this.todos = [ ];
+        return this.getAll();
+    };
+    this.getAll = function(){
+        return this.todos;
+    };
+    this.removeItem = function(item){
+        this.todos = this.todos.filter(function(todo){
+            return todo !== item;
+        });
+        return this.getAll();
+    };
+});
+
+app.controller("todoController", function($scope, $http, todoService, personService){
     // colores
 
     /* Funciona pero trae muchos resultados y traba el navegador
     $scope.images = [] ;
-
     $http({
         method: 'GET',
         url: 'http://jsonplaceholder.typicode.com/photos'
@@ -46,38 +102,51 @@ app.controller("todoController", function($scope, $http, localStorageService){
         });
     }
 
-    // Personas
+    // Este es el ejercicoi de personas
+    // implementado mediante un service
+    $scope.todo = { };
 
-    $scope.todos = [{ }];
+    $scope.todos = personService.getAll();
 
     $scope.addTodo = function(){
-        console.log($scope.todo);
-        $scope.todos.push($scope.todo);
-        $scope.todo = {} ;
+        personService.getAll();
+        personService.add($scope.todo);
+        $scope.todo = { } ;
     }
 
-    // todo
-    if(localStorageService.get("clave-todo")){
-        $scope.tds = localStorageService.get("clave-todo");
-    }else{
-        $scope.tds = [ ];
-    }
+    $scope.cle = function(){
+        $scope.todos = personService.clean();
 
-    $scope.td = {} ;
+    };
+
+    $scope.rem = function(item){
+        $scope.todos = personService.removeItem(item);
+
+    };
+
+    // este es el ejercicio del todo
+    // implementado mediante una factory
+    $scope.td = { } ;
 
     $scope.addTd = function(){
-        $scope.tds.push($scope.td);
-        $scope.td = { } ;
-    }
+        todoService.getAll();
+        todoService.add($scope.td);
+        $scope.td = { };
+    };
 
     $scope.clean = function(){
-        $scope.tds = [] ;
+        $scope.tds = todoService.clean();
 
-    }
+    };
 
-    $scope.$watchCollection('tds', function(newValue, oldValue){
-        localStorageService.set("clave-todo", $scope.tds);
-    });
+    $scope.remove = function(item){
+        $scope.tds = todoService.removeItem(item);
+
+    };
+
+    $scope.tds = todoService.getAll();
+
+    // aplicando filtros
 
     $scope.mi_html = "<p>aqui hay texto html que no se va a ver</p>"
 
@@ -90,5 +159,3 @@ app.filter("delChar", function(){
         return String(texto).replace(/<[^>]+>/gm,'');
     }
 })
-
-
